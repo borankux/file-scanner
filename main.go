093ef54eb044b0b7de25d87c43e7f7b4ddba10fd
Frank <mablat@ebay.com>
2022-03-30 22:54:47 +0800

    op

--- main.go
+++ main.go
@@ -11,16 +11,19 @@ import (
 
 var counter uint64
 
-func scan(path string, group *sync.WaitGroup) {
+func scan(path string, group *sync.WaitGroup, quite bool) {
 	dirs, _ := os.ReadDir(path)
 	for _, d := range dirs {
 		newPath := path + "/" + d.Name()
 		if d.IsDir() {
 			group.Add(1)
-			go scan(newPath, group)
+			go scan(newPath, group, quite)
 		} else {
 			atomic.AddUint64(&counter, 1)
-			fmt.Println(newPath)
+
+			if !quite {
+				fmt.Println(newPath)
+			}
 		}
 	}
 	group.Done()
@@ -32,12 +35,17 @@ func main() {
 		return
 	}
 
+	quite := true
+	if len(os.Args) == 2 {
+		quite = false
+	}
+
 	path := os.Args[1]
 	absPath, _ := filepath.Abs(path)
 	now := time.Now()
 	wg := sync.WaitGroup{}
 	wg.Add(1)
-	go scan(absPath, &wg)
+	go scan(absPath, &wg, quite)
 	wg.Wait()
 	diff := time.Since(now)
 	fmt.Printf("%d files, scanned at:%v\n", counter, diff)
